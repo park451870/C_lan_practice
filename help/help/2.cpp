881efeb0159e8442ba485a4669a26016008f9803
park451870 <74594132+park451870@users.noreply.github.com>
2020-11-17 21:47:33 +0900

    start
    start

---
+++ help/help/2.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+long long sum(int* a, int n) {
+	int i;
+	long long ans = 0;
+
+	for (i = 0; i < n; i++) {
+		ans += a[i];
+	}
+	return ans;
+}
+
+int main(void)
+{
+	
+}
